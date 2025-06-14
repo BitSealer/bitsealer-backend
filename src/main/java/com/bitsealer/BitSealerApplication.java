3d99ce1e91d9ad5e4b572df315edb5582a036be6
Pablo <pablonavarreitor@gmail.com>
2025-06-14 17:24:07 +0200

    Inicial commit: Haces un curl y te devuelve el codigo hash del archivo.

---
+++ src/main/java/com/bitsealer/BitSealerApplication.java
@@ -0,0 +1,13 @@
+package com.bitsealer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitSealerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitSealerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bitsealer/BitSealerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitsealer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitSealerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
